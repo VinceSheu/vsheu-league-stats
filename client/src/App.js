748c435aa78837df61c03ad687c60a1ed089ba00
Vincent Sheu <vincent.sheu@ttt.studio>
2019-05-03 22:25:55 -0700

    input validation and error styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import XRegExp from 'xregexp';
 
 class App extends Component {
   // Initialize state
   state = { value: '', matchHistory: [], loading: false, error: null }
 
   componentDidMount() {
-    //this.getMatchHistory();
+    
   }
 
   getMatchHistory = () => {
-    this.setState({ loading: true, error: null });
+    this.setState({ matchHistory: [], loading: true, error: null });
     fetch(`/api/matchHistory?username=${this.state.value}`)
       .then(res => {
           if (res.ok) {
@@ -30,7 +31,13 @@ class App extends Component {
   }
 
   onChange = (event) => {
-    this.setState({ value: event.target.value });
+    const value = event.target.value;
+    const regex = XRegExp('^[0-9\\pL _\\.]+$', 'gi');
+    let newInputError = null;
+    if (value !== '' && !regex.test(value)) {
+      newInputError = 'Input contains forbidden characters.';
+    }
+    this.setState({ value, error: newInputError });
   }
 
   renderMatchHistory = () => {
@@ -57,16 +64,27 @@ class App extends Component {
   }
 
   render() {
-    const { value, matchHistory, loading } = this.state;
+    const { value, matchHistory, loading, error } = this.state;
 
     return (
       <div className="App">
         <h1>League Stats</h1>
-        <input type="text" value={value} onChange={this.onChange} />
-        <button onClick={this.handleSubmit} disabled={loading}> Submit </button>
+        <input type="text"
+              className="input-field" 
+              value={value} 
+              placeholder="Enter a summoner name" 
+              onChange={this.onChange} 
+        />
+        <button onClick={this.handleSubmit} disabled={loading || error }> Submit </button>
+        {error && <div className="error">{error}</div>}
         <div className="content-area">
         { loading && <div className="loader"></div> }
-        { !loading && matchHistory && matchHistory.length > 0 && (this.renderMatchHistory()) }
+        { !loading && matchHistory && matchHistory.length > 0 && (
+          <div>
+            <h3>These are <b>{value}</b>'s last 10 matches:</h3>
+            {this.renderMatchHistory()}
+          </div>
+        ) }
         </div>
       </div>
     );
