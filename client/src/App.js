d4b2ced9c6325c634d9f7b70f1638702e59f5a5c
Vincent Sheu <vincent.sheu@ttt.studio>
2019-05-03 21:35:13 -0700

    rough working version

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,71 @@ import './App.css';
 
 class App extends Component {
   // Initialize state
-  state = { matchHistory: [] }
+  state = { value: '', matchHistory: [], loading: false, error: null }
 
   componentDidMount() {
-    this.getMatchHistory();
+    //this.getMatchHistory();
   }
 
   getMatchHistory = () => {
-    fetch(`/api/matchHistory`)
+    this.setState({ loading: true, error: null });
+    fetch(`/api/matchHistory?username=${this.state.value}`)
       .then(res => {
           if (res.ok) {
            return res.json()
           }
           throw res.statusText; 
         })
-      .then(data => this.setState({ matchHistory: data }))
+      .then(data => this.setState({ matchHistory: data.results, loading: false }))
+      .catch(err => {
+        this.setState({loading: false, error: err})
+        console.log(err);
+      });
+  }
+
+  handleSubmit = () => {
+    this.getMatchHistory();
+  }
+
+  onChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  renderMatchHistory = () => {
+    return this.state.matchHistory.map( match =>
+      <div 
+        key={match.gameId} 
+        className={`match-container ${match.didWin ? 'victory-container' : 'defeat-container'}`}>
+        { match.didWin ? (<div className="victory-font win-status">Victory</div>) : 
+          (<div className="defeat-font win-status">Defeat</div>)
+        }
+        <div className="level">{ `Level ${match.championLevel }`}</div>
+        <div className="duration">{ `${match.gameDuration}`}</div>
+        <div className="summoner-name">{ `${match.summonerName} as `} <b>{ match.championName }</b> </div>
+        <div className="kill-death">{ `KDA: ${match.kills}/${match.deaths}/${match.assists}` }</div>
+        <div className="cs">{ `CS: ${match.creepScore} (${match.creepPerMin})` }</div>
+        <div className="summonerSpells">{ `${match.summonerSpells[0]} & ${match.summonerSpells[1]}` }</div>
+        <div className="items-header">Items</div>
+        <div className="items">{ match.boughtItems.map( 
+            item => <div key={`${match.gameId} ${item}`} className="item">{item}</div>
+          )}
+        </div>
+      </div>
+    )
   }
 
   render() {
+    const { value, matchHistory, loading } = this.state;
 
     return (
       <div className="App">
+        <h1>League Stats</h1>
+        <input type="text" value={value} onChange={this.onChange} />
+        <button onClick={this.handleSubmit} disabled={loading}> Submit </button>
+        <div className="content-area">
+        { loading && <div className="loader"></div> }
+        { !loading && matchHistory && matchHistory.length > 0 && (this.renderMatchHistory()) }
+        </div>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,17 +1,103 @@
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
+const { Kayn, REGIONS } = require('kayn');
 
 const app = express();
+const kayn = Kayn(process.env.RIOT_API_KEY)();
 
+const processMatch = (championIdMap, items, summonerSpells, summonerId, match) => {
+
+  const { participantId, player } = match.participantIdentities.find(
+      pi => pi.player.summonerId === summonerId,
+  )
+  const {championId, stats, spell1Id, spell2Id} = match.participants.find(
+      p => p.participantId === participantId,
+  )
+
+  // extract summonerSpells
+  const selectedSummonerSpells = ['N/A', 'N/A'];
+  Object.values(summonerSpells).forEach(spell => {
+    if (spell.key === spell1Id.toString()) selectedSummonerSpells[0] = spell.name;
+    if (spell.key === spell2Id.toString()) selectedSummonerSpells[1] = spell.name;
+  })
+
+  // extract bought items
+  const boughtItems = [
+    items[stats.item0] && items[stats.item0].name, 
+    items[stats.item1] && items[stats.item1].name, 
+    items[stats.item2] && items[stats.item2].name,
+    items[stats.item3] && items[stats.item3].name,
+    items[stats.item4] && items[stats.item4].name,
+    items[stats.item5] && items[stats.item5].name,
+  ];
+
+  //extract gameDuration and also the human-readable form
+  const gameDurationInMinutes = (match.gameDuration/60);
+  const minutesOnly = gameDurationInMinutes.toFixed(0);
+  const remainderSeconds = (match.gameDuration % 60);
+
+  const champion = championIdMap.data[championId]
+
+  return {
+      gameId: match.gameId,
+      didWin: stats.win,
+      gameDuration: `${minutesOnly}m ${remainderSeconds}s`,
+      summonerName: player.summonerName,
+      summonerSpells: selectedSummonerSpells,
+      //perks
+      kills: stats.kills,
+      deaths: stats.deaths,
+      assists: stats.assists,
+      championName: champion.name,
+      boughtItems,
+      championLevel: stats.champLevel,
+      creepScore: stats.totalMinionsKilled,
+      creepPerMin: (stats.totalMinionsKilled / gameDurationInMinutes).toFixed(1)
+  }
+}
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/api/matchHistory', (req, res) => {
+app.get('/api/matchHistory', async (req, res) => {
+  
+  try {
+    const { id: summonerId , accountId } = await kayn.Summoner.by.name(req.query.username);
+    const { matches } = await kayn.Matchlist.by
+          .accountID(accountId);
+    const gameIds = matches.slice(0, 10).map(({ gameId }) => gameId);
+    const matchDetails = await Promise.all(gameIds.map(kayn.Match.get));
 
-  const testArray = Array.from(Array(5).keys()).map(i =>i);
+    const championIdMap = await kayn.DDragon.Champion.listDataByIdWithParentAsId();
+    const { data: items }  =  await kayn.DDragon.Item.list();
+    const { data: summonerSpells } = await kayn.DDragon.SummonerSpell.list();
+    //const perks  = await kayn.DDragon.RunesReforged.list(); bugged?
 
-  res.json(testArray);
+    const results = await Promise.all(matchDetails.map(
+      match => processMatch(championIdMap, items, summonerSpells, summonerId, match)
+    ));
 
+    const newRes = {
+      matches,
+      matchDetails,
+      items,
+      results
+    }
+
+    res.json(newRes);
+
+    console.log('------');
+  }
+  catch(err) {
+    switch (err.statusCode) {
+      case 404:  {
+        res.statusMessage = 'Summoner Not Found';
+        res.sendStatus(404);
+      }
+    }
+    console.log(err);
+  }
+  
 });
 
 // The "catchall" handler: for any request that doesn't
